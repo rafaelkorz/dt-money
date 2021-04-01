2289346a9a3e0404419c117f03810237bba5bc8a
Rafael Korz <rafael.korz@gmail.com>
2021-03-31 23:53:14 -0300

    Finished the modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsTable } from './components/TransactionsTable';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
+  const [isNewTransactionModalOpen, SetIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    SetIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    SetIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />      
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <GlobalStyle />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} />
+      <TransactionsTable />
     </>
   );
 }
